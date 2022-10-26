d4e02dbda5c39d42a4fbc545e51e9d34fc33f3a4
BoraKol <borakol07@gmail.com>
2022-10-26 16:30:48 +0300

    Odev commiti

---
+++ app_server/controllers/digerleri.js
@@ -0,0 +1,9 @@
+const hakkinda=function(req, res, next) {
+    res.render('hakkinda', { title: 'Hakkında Sayfası' });
+}
+module.exports={
+    hakkinda
+}
+//yorum
+//odev3 denemesi
+//Replit degisikligi2
